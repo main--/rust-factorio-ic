cd94d07d47f54aea1add3e5dcd0d95fc15f32bbb
main() <main@ehvag.de>
2020-05-02 18:20:07 +0200

    more fixes

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     let recipes = recipe::extract_recipes(path).unwrap();
     println!("Parsed {} recipes", recipes.len());
 
-    let tree = kirkmcdonald::kirkmcdonald(&recipes, "chemical-science-pack", 0.1);
+    let tree = kirkmcdonald::kirkmcdonald(&recipes, "logistic-science-pack", 0.75);
     println!("{:#?}", tree);
     let needed_assemblers: Vec<_> = kirkmcdonald::needed_assemblers(&tree).collect();
     println!("assemblers needed: {:?}", needed_assemblers);
@@ -55,4 +55,7 @@ fn main() {
 
     routing::route(&mut pcb, &mut needed_wires, routing::lee_pathfinder);
     //routing::route(&mut pcb, &mut needed_wires, routing::mylee);
+
+    println!("{}", render::blueprint(&pcb));
+    println!("{}", render::ascii(&pcb));
 }
--- src/routing/mod.rs
+++ src/routing/mod.rs
@@ -16,24 +16,35 @@ pub fn route(pcb: &mut Pcb, needed_wires: &mut NeededWires, pathfinder_fn: fn(&m
     let mut temperature = 20;
     let mut rng = StdRng::from_seed([0; 32]);
 
-    while let Err(i) = try_wiring(pcb.clone(), &needed_wires, pathfinder_fn) {
-        let ele = needed_wires.remove(i);
-        needed_wires.insert(0, ele);
+    loop {
+        match try_wiring(pcb.clone(), &needed_wires, pathfinder_fn) {
+            Ok(p) => {
+                *pcb = p;
+                return;
+            }
+            Err(i) => {
+                let ele = needed_wires.remove(i);
+                needed_wires.insert(0, ele);
 
-        if panic == temperature {
-            panic = 0;
-            temperature += 1;
+                if panic == temperature {
+                    panic = 0;
+                    temperature += 1;
 
-            needed_wires.shuffle(&mut rng);
+                    needed_wires.shuffle(&mut rng);
+                }
+
+                panic += 1;
+                println!("panic={}", panic);
+            }
         }
-
-        panic += 1;
-        println!("panic={}", panic);
     }
 }
 
 #[throws(usize)]
-fn try_wiring(mut pcb: Pcb, needed_wires: &NeededWires, pathfinder_fn: fn(&mut Pcb, (i32, i32), (i32, i32)) -> Result<(), ()>) {
+fn try_wiring(mut pcb: Pcb,
+              needed_wires: &NeededWires,
+              pathfinder_fn: fn(&mut Pcb, (i32, i32), (i32, i32)) -> Result<(), ()>)
+        -> Pcb {
     for (i, &(from, to)) in needed_wires.iter().enumerate() {
         // render_blueprint_ascii(&pcb);
         pathfinder_fn(&mut pcb, from, to).map_err(|()| i)?;
@@ -41,6 +52,8 @@ fn try_wiring(mut pcb: Pcb, needed_wires: &NeededWires, pathfinder_fn: fn(&mut P
 
 //    println!("{}", render::ascii(&pcb));
 //    println!("{}", render::blueprint(&pcb));
+
+    pcb
 }
 
 enum Belt {
